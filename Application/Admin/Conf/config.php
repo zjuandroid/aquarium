bdbcaa4eb0b063f0c71114b5f548fe7427251dbe
zjuandroid <zjuwangchao@126.com>
2016-07-08 22:02:56 +0800

    success error

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -3,12 +3,7 @@ return array(
 	//'配置项'=>'配置值'
     //主题静态文件路径
     'TMPL_PARSE_STRING' => array(
-        '__STATIC__' => __ROOT__.'/Application/'.MODULE_NAME.'/View/' . '/Public/static',
-            //TODO在此设置常量不起作用
-//            'PAGE_COUNT' => 10,
-//        'SHOW_PAGE_TRACE'=>true,
-        //TODO在此设置常量不起作用
-//        'APP_KEY_ANDROID' => '575e335ae0f55a1141001196',
-//        'APP_MASTER_SECRET_ANDROID' => 'lbfmvyhrerw5iwjrnigakngyipuqrjdy'
-        ),
+        '__STATIC__' => __ROOT__.'/Application/'.MODULE_NAME.'/View/' . '/Public/static',   ),
+    'TMPL_ACTION_SUCCESS'=>'Public:dispatch_jump',
+    'TMPL_ACTION_ERROR'=>'Public:dispatch_jump',
 );
